6f55fd8783f26e2a345b8ab647e49e0470de84c5
CodeMuse <codemuse@CodeMuses-MacBook-Air.local>
2023-06-16 14:15:01 +0530

    deleted signup file from api

--- api/signup.js
+++
@@ -1,8 +0,0 @@
-import { cookies } from 'next/headers'
-
-export async function handleSignUp(data) {
-    'use server';
-    const user = cookies().get('data')?.value
-    console.log(user,'api cookies');
-  }
- 
